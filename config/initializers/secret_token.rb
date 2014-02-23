ed8e7b49a12c7e29ccd5099e5a48268666ddbab1
Ben Pellow <benjamin.pellow@gmail.com>
2014-02-22 21:11:19 -0800

    rails new addons-gem-test :yum:

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AddonsGemTest::Application.config.secret_key_base = '1e7b7fb2fe1eb45c3595c7575151fcf004a325c5700f9216d3460e4d474ca5897ebcb4c1e230458ac5640ede6bdc2d7be281150f9f707957621f37b4e7af5faa'
